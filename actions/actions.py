615adbafb3574611757f5649d5d2589741896804
Felix Rapp <felix@MacBook-Pro.fritz.box>
2022-06-28 00:10:10 +0200

    added validation action, added location intent and much more

--- actions/actions.py
+++ actions/actions.py
@@ -1,11 +1,18 @@
 import datetime as dt
 from typing import Any, Text, Dict, List
 
+from rasa_sdk.events import SlotSet
+from rasa_sdk import Action, Tracker, FormValidationAction
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.types import DomainDict
+import pathlib
+
+ALLOWED_PIZZA_TYPES = ["margaritha", "salami", "prosciutto", "quattro formaggi"]
+ALLOWED_PIZZA_SIZES = ["small", "medium", "large"]
 
 
-class ActionHelloWorld(Action):
+class ActionShowTime(Action):
 
     def name(self) -> Text:
         return "action_show_time"
@@ -15,7 +22,6 @@ class ActionHelloWorld(Action):
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
         dispatcher.utter_message(text=f"{dt.datetime.now()}")
-
         return []
 
 class ActionRepeatOrder(Action):
@@ -29,9 +35,40 @@ class ActionRepeatOrder(Action):
 
         pizza_type = tracker.get_slot("pizza_type")
         pizza_size = tracker.get_slot("pizza_size")
-        if not pizza_type:
-            dispatcher.utter_message(text="I don't know your order.")
+        if not pizza_type or not pizza_size:
+            dispatcher.utter_message(text=f"I don't know your order.")
         else:
-            dispatcher.utter_message(text=f"Your ordered a {pizza_size} pizza {pizza_type}!")
-
+            dispatcher.utter_message(text=f"Your ordered a {pizza_size} size pizza {pizza_type}!")
         return []
+
+class ValidatePizzaForm(FormValidationAction):
+    def name(self) -> Text:
+        return "validate_pizza_form"
+
+    def validate_pizza_type(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        """Validate `pizza_type` value."""
+
+        if slot_value.lower() not in ALLOWED_PIZZA_TYPES:
+            dispatcher.utter_message(text=f"We don't offer this pizza. You can choose between Pizza Margaritha, Pizza Salami, Pizza Prosciutto and Pizza Quattro Formaggi.")
+            return {"pizza_type": None}
+        dispatcher.utter_message(text=f"{slot_value} is a great choice!.")
+        return {"pizza_type": slot_value}
+
+    def validate_pizza_size(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        """Validate `pizza_size` value."""
+
+        if slot_value.lower() not in ALLOWED_PIZZA_SIZES:
+            dispatcher.utter_message(text=f"We sadly don't offer the requested size.")
+            return {"pizza_size": None}
+        dispatcher.utter_message(text=f"You picked a {slot_value} pizza.")
+        return {"pizza_size": slot_value}
